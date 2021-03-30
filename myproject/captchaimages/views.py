208116ece872d7b8df05f4da9c9ffa468fa7d2c0
HongHanh120 <lehonghanh.120@gmail.com>
2021-03-30 14:22:55 +0700

    refresh image

--- myproject/captchaimages/views.py
+++ myproject/captchaimages/views.py
@@ -3,7 +3,6 @@ from django.template.response import TemplateResponse
 from django.http import HttpResponse, JsonResponse
 
 import sys
-import json
 from urllib.parse import urlparse
 from subprocess import run, PIPE
 from django.core.files.base import ContentFile
@@ -16,7 +15,7 @@ def home(request):
     return render(request, "base.html")
 
 
-def external(request):
+def upload(request):
     out = run([sys.executable, '//home//hanh//Desktop//captcha//mass_captcha//generate_mass_captcha.py'], shell=False,
               stdout=PIPE)
 
@@ -35,15 +34,19 @@ def external(request):
         image_data = f.read()
     image.image.save(name, ContentFile(image_data))
 
-    if request.method == 'GET':
-        image = Image.objects.get(text=text)
+    return image
 
+
+def display(request):
+    image = upload(request)
     image_url = image.image.url
-    data = {
-        'url': image_url
-    }
-    dataJSON = json.dumps(data)
-    print(dataJSON)
-    print(image.image.url)
 
-    return render(request, 'includes/captcha.html', {'data': dataJSON, 'image_url': image.image.url})
+    if request.method == "GET":
+        if request.is_ajax():
+            return JsonResponse({'image_url': image_url}, safe=False)
+        else:
+            context = {
+                'image_url': image_url
+            }
+            return render(request, 'includes/captcha.html', context)
+
